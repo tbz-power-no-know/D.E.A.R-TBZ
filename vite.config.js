b595c31812fcc17ec519f7918523e40290e9d937
tbz-power-no-know <antoine.perchaud@threema.ch>
2026-06-18 13:50:47 +0200

    init commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import { readdirSync } from 'fs'
+import { resolve } from 'path'
+
+const pagesDir = new URL('./pages', import.meta.url).pathname
+
+const input = Object.fromEntries(
+  readdirSync(pagesDir)
+    .filter(f => f.endsWith('.html'))
+    .map(f => [f.replace('.html', ''), resolve(pagesDir, f)])
+)
+
+export default defineConfig({
+  root: 'pages',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: { input }
+  }
+})
