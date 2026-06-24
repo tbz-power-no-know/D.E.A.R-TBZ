1a19637fd7ff5cae69ac75608a653ca86fac27b7
Developer <dev@local>
2026-06-24 07:25:15 +0000

    refactor: move header/footer to JS modules and clean up src/
    - Add pages/js/shared/Header.js and Footer.js components
    - Add pages/js/main.js entry point
    - Remove empty src/shared/ files and src/main.js
    - Add footer styles to main.css

---
+++ pages/js/main.js
@@ -0,0 +1,7 @@
+import { renderHeader } from './shared/Header.js';
+import { renderFooter } from './shared/Footer.js';
+import { initExpandable } from './expandable.js';
+
+renderHeader();
+renderFooter();
+initExpandable();
