386a981a9d090da8e7cef8bc44429f534f2d5b9f
tbz-power-no-know <dev@local>
2026-06-24 08:40:28 +0000

    refactor: wire home page rendering into main.js
    - Auto-initialize latest podcasts and categories on home page
    - Conditional init based on element presence

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,7 +1,16 @@
-import { renderHeader } from './shared/Header.js';
-import { renderFooter } from './shared/Footer.js';
-import { initExpandable } from './expandable.js';
+import { renderHeader } from './shared/Header.js'
+import { renderFooter } from './shared/Footer.js'
+import { initExpandable } from './expandable.js'
+import { renderLatestPodcasts, renderCategories } from './home.js'
 
-renderHeader();
-renderFooter();
-initExpandable();
+renderHeader()
+renderFooter()
+initExpandable()
+
+if (document.getElementById('latest-podcasts')) {
+  renderLatestPodcasts(document.getElementById('latest-podcasts'))
+}
+
+if (document.getElementById('categories-list')) {
+  renderCategories(document.getElementById('categories-list'))
+}
